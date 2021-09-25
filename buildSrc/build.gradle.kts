34d1fe7fa92b09513df6ea765a116b7770b2c128
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-09-25 12:48:08 +0200

    Add `buildSrc`

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // TODO: remove when the next issue is fixed:
+    //  https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
+    implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+
+    implementation(libs.kotlin.kotlinGradlePluginx)
+}
