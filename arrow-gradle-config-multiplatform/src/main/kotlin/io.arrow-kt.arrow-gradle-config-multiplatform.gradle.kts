0bd4af71ca6ab697f80e4b3ce3e3db565a79cfb7
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-09-30 13:19:58 +0200

    Add all Kotlin Multiplatform targets

--- arrow-gradle-config-multiplatform/src/main/kotlin/io.arrow-kt.arrow-gradle-config-multiplatform.gradle.kts
+++ arrow-gradle-config-multiplatform/src/main/kotlin/io.arrow-kt.arrow-gradle-config-multiplatform.gradle.kts
@@ -7,27 +7,34 @@ plugins {
 
 configure<KotlinMultiplatformExtension> {
     explicitApi()
+
     jvm {
-        // JVM target ignores Java sources and compiles only Kotlin source files.
-        // Fix:
+        // Fix JVM target ignores Java sources and compiles only Kotlin source files.
         withJava()
     }
     js(IR) {
         browser()
         nodejs()
     }
+
     linuxX64()
 
     mingwX64()
 
-    macosX64()
+    iosArm32()
+    iosArm64()
+    iosSimulatorArm64()
+    iosX64()
     macosArm64()
-
-    tvos()
-
-    watchos()
-
-    ios()
+    macosX64()
+    tvosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    watchosArm32()
+    watchosArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosX86()
 
     targets.all {
         compilations.all {
@@ -39,42 +46,79 @@ configure<KotlinMultiplatformExtension> {
 
     sourceSets {
         val commonMain by getting
-        val macosX64Main by getting
-        val macosArm64Main by getting
         val mingwX64Main by getting
         val linuxX64Main by getting
-        val iosMain by getting
-        val watchosMain by getting
-        val tvosMain by getting
-
-        val commonTest by getting
-        val macosX64Test by getting
-        val macosArm64Test by getting
-        val mingwX64Test by getting
-        val linuxX64Test by getting
-        val iosTest by getting
-        val watchosTest by getting
-        val tvosTest by getting
+        val iosArm32Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosX64Main by getting
+        val macosArm64Main by getting
+        val macosX64Main by getting
+        val tvosArm64Main by getting
+        val tvosSimulatorArm64Main by getting
+        val tvosX64Main by getting
+        val watchosArm32Main by getting
+        val watchosArm64Main by getting
+        val watchosSimulatorArm64Main by getting
+        val watchosX64Main by getting
+        val watchosX86Main by getting
 
         create("nativeMain") {
             dependsOn(commonMain)
-            macosX64Main.dependsOn(this)
-            macosArm64Main.dependsOn(this)
             mingwX64Main.dependsOn(this)
             linuxX64Main.dependsOn(this)
-            iosMain.dependsOn(this)
-            watchosMain.dependsOn(this)
-            tvosMain.dependsOn(this)
+            iosArm32Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            iosX64Main.dependsOn(this)
+            macosArm64Main.dependsOn(this)
+            macosX64Main.dependsOn(this)
+            tvosArm64Main.dependsOn(this)
+            tvosSimulatorArm64Main.dependsOn(this)
+            tvosX64Main.dependsOn(this)
+            watchosArm32Main.dependsOn(this)
+            watchosArm64Main.dependsOn(this)
+            watchosSimulatorArm64Main.dependsOn(this)
+            watchosX64Main.dependsOn(this)
+            watchosX86Main.dependsOn(this)
         }
+
+        val commonTest by getting
+        val mingwX64Test by getting
+        val linuxX64Test by getting
+        val iosArm32Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosX64Test by getting
+        val macosArm64Test by getting
+        val macosX64Test by getting
+        val tvosArm64Test by getting
+        val tvosSimulatorArm64Test by getting
+        val tvosX64Test by getting
+        val watchosArm32Test by getting
+        val watchosArm64Test by getting
+        val watchosSimulatorArm64Test by getting
+        val watchosX64Test by getting
+        val watchosX86Test by getting
+
         create("nativeTest") {
             dependsOn(commonTest)
-            macosX64Test.dependsOn(this)
-            macosArm64Test.dependsOn(this)
             mingwX64Test.dependsOn(this)
             linuxX64Test.dependsOn(this)
-            iosTest.dependsOn(this)
-            watchosTest.dependsOn(this)
-            tvosTest.dependsOn(this)
+            iosArm32Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+            iosX64Test.dependsOn(this)
+            macosArm64Test.dependsOn(this)
+            macosX64Test.dependsOn(this)
+            tvosArm64Test.dependsOn(this)
+            tvosSimulatorArm64Test.dependsOn(this)
+            tvosX64Test.dependsOn(this)
+            watchosArm32Test.dependsOn(this)
+            watchosArm64Test.dependsOn(this)
+            watchosSimulatorArm64Test.dependsOn(this)
+            watchosX64Test.dependsOn(this)
+            watchosX86Test.dependsOn(this)
         }
     }
 }
