d070e1359422e7ad128a1d86293ef7b1062a2fc9
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-10-02 01:43:03 +0200

    Remove unnecessary elements on `publish-java-platform`

--- arrow-gradle-config-publish-java-platform/src/main/kotlin/io.arrow-kt.arrow-gradle-config-publish-java-platform.gradle.kts
+++ arrow-gradle-config-publish-java-platform/src/main/kotlin/io.arrow-kt.arrow-gradle-config-publish-java-platform.gradle.kts
@@ -4,23 +4,8 @@ import io.arrow.gradle.core.publishing.signPublications
 plugins {
     `maven-publish`
     signing
-    id("org.jetbrains.dokka")
 }
 
-val docsJar by project.tasks.creating(Jar::class) {
-    group = "build"
-    description = "Assembles Javadoc jar file from for publishing"
-    archiveClassifier.set("javadoc")
-    from(tasks.dokkaJavadoc)
-}
-
-val sourcesJar by project.tasks.creating(Jar::class) {
-    group = "build"
-    description = "Assembles Sources jar file for publishing"
-    archiveClassifier.set("sources")
-    from((project.properties["sourceSets"] as SourceSetContainer)["main"].allSource)
-}
-
-setupPublishing(docsJar, sourcesJar, publishFromJavaPlatform = true)
+setupPublishing(publishFromJavaPlatform = true)
 
 signPublications()
