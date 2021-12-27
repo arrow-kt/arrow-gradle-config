08d15b66534bfe6edce8bcae2686485a4986225e
Simon Vergauwen <nomisRev@users.noreply.github.com>
2021-12-27 13:17:29 +0100

    Parameterises the url for nexus

--- arrow-gradle-config-nexus/src/main/kotlin/io.arrow-kt.arrow-gradle-config-nexus.gradle.kts
+++ arrow-gradle-config-nexus/src/main/kotlin/io.arrow-kt.arrow-gradle-config-nexus.gradle.kts
@@ -12,6 +12,17 @@ configure<NexusPublishExtension> {
       stagingProfileId.set(
         "${properties["oss.stagingProfileId"] ?: System.getenv("OSS_STAGING_PROFILE_ID")}",
       )
+      repositoryRelease?.let { nexusUrl.set(uri(it)) }
+      repositorySnapshot?.let { snapshotRepositoryUrl.set(uri(it)) }
     }
   }
 }
+
+fun Project.getVariableOrNull(propertyName: String, environmentVariableName: String): String? =
+  project.properties[propertyName]?.toString() ?: System.getenv(environmentVariableName)
+
+val Project.repositorySnapshot: String?
+  get() = getVariableOrNull("repository.snapshot", "REPOSITORY_SNAPSHOT")
+
+val Project.repositoryRelease: String?
+  get() = getVariableOrNull("repository.release", "REPOSITORY_RELEASE")
