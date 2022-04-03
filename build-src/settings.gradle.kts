574aa781fd017292bd4f4cbe28727b456550392e
i-walker <46971368+i-walker@users.noreply.github.com>
2022-04-03 13:33:38 +0200

    update dependencies

--- build-src/settings.gradle.kts
+++ build-src/settings.gradle.kts
@@ -4,6 +4,8 @@ dependencyResolutionManagement {
   versionCatalogs {
     create("libs") {
       from(files("../gradle/libs.versions.toml"))
+      val kotlinVersion: String? by settings
+      kotlinVersion?.let { version("kotlin", it) }
     }
   }
 }
