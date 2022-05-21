adf2b5879375856eaf973f2ca56ff5130b6b9755
i-walker <46971368+i-walker@users.noreply.github.com>
2022-05-21 23:56:44 +0200

    add dokka setup to be optional

--- arrow-gradle-config-publish/src/main/kotlin/internal/ConfigureDokka.kt
+++ arrow-gradle-config-publish/src/main/kotlin/internal/ConfigureDokka.kt
@@ -6,36 +6,47 @@ import org.gradle.kotlin.dsl.withType
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 
-fun Project.configureDokka() {
-  afterEvaluate {
-    val baseUrl: String = checkNotNull(properties["pom.smc.url"]?.toString())
+fun Project.configureDokka(): Unit =
+  if (dokkaEnabled) {
+    afterEvaluate {
+      val baseUrl: String = checkNotNull(properties["pom.smc.url"]?.toString())
 
-    tasks.withType<DokkaTask>().configureEach {
-      outputDirectory.set(
-        file(dokkaOutputDirectory ?: "${rootProject.rootDir}/arrow-site/docs/apidocs")
-      )
-      extensions.findByType<KotlinProjectExtension>()?.sourceSets?.forEach { kotlinSourceSet ->
-        dokkaSourceSets.named(kotlinSourceSet.name) {
-          perPackageOption {
-            matchingRegex.set(".*\\.internal.*") // match all .internal packages and sub-packages
-            suppress.set(true)
-          }
-          skipDeprecated.set(true)
-          reportUndocumented.set(false)
-          kotlinSourceSet.kotlin.srcDirs.forEach { srcDir ->
-            sourceLink {
-              localDirectory.set(srcDir)
-              remoteUrl.set(uri("$baseUrl/${srcDir.relativeTo(rootProject.rootDir)}").toURL())
-              remoteLineSuffix.set("#L")
+      tasks.withType<DokkaTask>().configureEach {
+        outputDirectory.set(
+          file(dokkaOutputDirectory ?: "${rootProject.rootDir}/arrow-site/docs/apidocs")
+        )
+        extensions.findByType<KotlinProjectExtension>()?.sourceSets?.forEach { kotlinSourceSet ->
+          dokkaSourceSets.named(kotlinSourceSet.name) {
+            perPackageOption {
+              matchingRegex.set(".*\\.internal.*") // match all .internal packages and sub-packages
+              suppress.set(true)
+            }
+            skipDeprecated.set(true)
+            reportUndocumented.set(false)
+            kotlinSourceSet.kotlin.srcDirs.forEach { srcDir ->
+              sourceLink {
+                localDirectory.set(srcDir)
+                remoteUrl.set(uri("$baseUrl/${srcDir.relativeTo(rootProject.rootDir)}").toURL())
+                remoteLineSuffix.set("#L")
+              }
             }
           }
         }
       }
     }
+  } else {
+    tasks.withType<DokkaTask>().configureEach {
+      enabled = false
+    }
   }
-}
+
 
 val Project.dokkaOutputDirectory: String?
   get() =
     project.properties["dokka.outputDirectory"]?.toString()
       ?: System.getenv("DOKKA_OUTPUT_DIRECTORY")
+
+val Project.dokkaEnabled: Boolean
+  get() =
+    (project.properties["dokkaEnabled"]?.toString()
+      ?: "true").toBoolean()
--- arrow-gradle-config-publish/src/main/kotlin/io.arrow-kt.arrow-gradle-config-publish.gradle.kts
+++ arrow-gradle-config-publish/src/main/kotlin/io.arrow-kt.arrow-gradle-config-publish.gradle.kts
@@ -1,6 +1,7 @@
 import io.arrow.gradle.config.publish.arrowGradleConfigVersion
 import io.arrow.gradle.config.publish.internal.configureDokka
 import io.arrow.gradle.config.publish.internal.configurePublish
+import io.arrow.gradle.config.publish.internal.dokkaEnabled
 
 plugins {
   `maven-publish`
@@ -12,7 +13,9 @@ configurePublish()
 configureDokka()
 
 dependencies {
-  "dokkaGfmPlugin"(
-    "io.arrow-kt:arrow-gradle-config-dokka-fence-workaround:$arrowGradleConfigVersion"
-  )
+  if (dokkaEnabled) {
+    "dokkaGfmPlugin"(
+      "io.arrow-kt:arrow-gradle-config-dokka-fence-workaround:$arrowGradleConfigVersion"
+    )
+  }
 }
