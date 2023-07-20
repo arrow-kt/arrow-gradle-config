e7ee73aa4a685968ce7ef928d61b347933b9f542
Alejandro Serrano <trupill@gmail.com>
2023-07-20 10:10:09 +0200

    Bring list of supported Native platforms up-to-date (#74)
    * Bring list of supported Native platforms up-to-date
    * Update versions

--- arrow-gradle-config-kotlin/src/main/kotlin/io.arrow-kt.arrow-gradle-config-kotlin.gradle.kts
+++ arrow-gradle-config-kotlin/src/main/kotlin/io.arrow-kt.arrow-gradle-config-kotlin.gradle.kts
@@ -38,65 +38,73 @@ if (isKotlinMultiplatform) {
       nodejs()
     }
 
+    // Native: https://kotlinlang.org/docs/native-target-support.html
+    // -- Tier 1 --
     linuxX64()
-    linuxArm64()
-
-    mingwX64()
-
-    iosArm32()
-    iosArm64()
+    macosX64()
+    macosArm64()
     iosSimulatorArm64()
     iosX64()
-    macosArm64()
-    macosX64()
-    tvosArm64()
-    tvosSimulatorArm64()
-    tvosX64()
-    watchosArm32()
-    watchosArm64()
+    // -- Tier 2 --
+    linuxArm64()
     watchosSimulatorArm64()
     watchosX64()
-    watchosX86()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+    iosArm64()
+    // -- Tier 3 --
+    mingwX64()
+    // Android and watchOS not included
+    // -- Deprecated as of 1.8.20 --
+    // iosArm32() // deprecated as of 1.8.20
+    // watchosX86()
 
     sourceSets {
       val commonMain by getting
-      val mingwX64Main by getting
+
+      // Native
+      // -- Tier 1 -- 
       val linuxX64Main by getting
-      val linuxArm64Main by getting
-      val iosArm32Main by getting
-      val iosArm64Main by getting
+      val macosX64Main by getting
+      val macosArm64Main by getting
       val iosSimulatorArm64Main by getting
       val iosX64Main by getting
-      val macosArm64Main by getting
-      val macosX64Main by getting
-      val tvosArm64Main by getting
-      val tvosSimulatorArm64Main by getting
-      val tvosX64Main by getting
-      val watchosArm32Main by getting
-      val watchosArm64Main by getting
+      // -- Tier 2 --
+      val linuxArm64Main by getting
       val watchosSimulatorArm64Main by getting
       val watchosX64Main by getting
-      val watchosX86Main by getting
+      val watchosArm32Main by getting
+      val watchosArm64Main by getting
+      val tvosSimulatorArm64Main by getting
+      val tvosX64Main by getting
+      val tvosArm64Main by getting
+      val iosArm64Main by getting
+      // -- Tier 3 --
+      val mingwX64Main by getting
 
       create("nativeMain") {
         dependsOn(commonMain)
-        mingwX64Main.dependsOn(this)
+        // -- Tier 1 --
         linuxX64Main.dependsOn(this)
-        linuxArm64Main.dependsOn(this)
-        iosArm32Main.dependsOn(this)
-        iosArm64Main.dependsOn(this)
+        macosX64Main.dependsOn(this)
+        macosArm64Main.dependsOn(this)
         iosSimulatorArm64Main.dependsOn(this)
         iosX64Main.dependsOn(this)
-        macosArm64Main.dependsOn(this)
-        macosX64Main.dependsOn(this)
-        tvosArm64Main.dependsOn(this)
-        tvosSimulatorArm64Main.dependsOn(this)
-        tvosX64Main.dependsOn(this)
-        watchosArm32Main.dependsOn(this)
-        watchosArm64Main.dependsOn(this)
+        // -- Tier 2 --
+        linuxArm64Main.dependsOn(this)
         watchosSimulatorArm64Main.dependsOn(this)
         watchosX64Main.dependsOn(this)
-        watchosX86Main.dependsOn(this)
+        watchosArm32Main.dependsOn(this)
+        watchosArm64Main.dependsOn(this)
+        tvosSimulatorArm64Main.dependsOn(this)
+        tvosX64Main.dependsOn(this)
+        tvosArm64Main.dependsOn(this)
+        iosArm64Main.dependsOn(this)
+        // -- Tier 3 --
+        mingwX64Main.dependsOn(this)
       }
     }
   }
