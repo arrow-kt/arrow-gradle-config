61f1318a03ed757a3265f374df194da62b42d4c7
Alejandro Serrano Mena <trupill@gmail.com>
2025-01-10 12:22:53 +0100

    Spotless

--- arrow-gradle-config-publish/src/main/kotlin/internal/JarTasks.kt
+++ arrow-gradle-config-publish/src/main/kotlin/internal/JarTasks.kt
@@ -11,41 +11,51 @@ import org.gradle.kotlin.dsl.register
 
 internal val Project.docsJar: Jar
   get() =
-    tasks.register("docsJar", fun Jar.() {
-      group = "build"
-      description = "Assembles Javadoc jar file from for publishing"
-      archiveClassifier.set("javadoc")
-      if (dokkaEnabled) {
-        tasks.findByName("dokkaHtml")?.let { dokkaHtml -> from(dokkaHtml) }
-      }
-    }).get()
+    tasks
+      .register(
+        "docsJar",
+        fun Jar.() {
+          group = "build"
+          description = "Assembles Javadoc jar file from for publishing"
+          archiveClassifier.set("javadoc")
+          if (dokkaEnabled) {
+            tasks.findByName("dokkaHtml")?.let { dokkaHtml -> from(dokkaHtml) }
+          }
+        }
+      )
+      .get()
 
 internal val Project.sourcesJar: Jar
   get() =
-    tasks.register("sourcesJar", fun Jar.() {
-      group = "build"
-      description = "Assembles Sources jar file for publishing"
-      archiveClassifier.set("sources")
+    tasks
+      .register(
+        "sourcesJar",
+        fun Jar.() {
+          group = "build"
+          description = "Assembles Sources jar file for publishing"
+          archiveClassifier.set("sources")
 
-      val sources: Iterable<File> =
-        when {
-          isVersionCatalog -> emptySet()
-          isKotlinMultiplatform -> emptySet()
-          isJavaPlatform -> emptySet()
-          isKotlinJvm || isGradlePlugin -> {
-            (project.properties["sourceSets"] as SourceSetContainer)["main"].allSource
-          }
-          /*
-          isAndroidLibrary -> {
-            (project.extensions.getByName("android") as LibraryExtension)
-              .sourceSets
-              .named("main")
-              .get()
-              .java
-              .srcDirs
-          }
-          */
-          else -> emptySet()
+          val sources: Iterable<File> =
+            when {
+              isVersionCatalog -> emptySet()
+              isKotlinMultiplatform -> emptySet()
+              isJavaPlatform -> emptySet()
+              isKotlinJvm || isGradlePlugin -> {
+                (project.properties["sourceSets"] as SourceSetContainer)["main"].allSource
+              }
+              /*
+              isAndroidLibrary -> {
+                (project.extensions.getByName("android") as LibraryExtension)
+                  .sourceSets
+                  .named("main")
+                  .get()
+                  .java
+                  .srcDirs
+              }
+              */
+              else -> emptySet()
+            }
+          if (sources.toList().isNotEmpty()) from(sources)
         }
-      if (sources.toList().isNotEmpty()) from(sources)
-    }).get()
+      )
+      .get()
