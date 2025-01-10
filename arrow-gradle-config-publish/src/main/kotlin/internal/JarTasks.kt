b273128e6366deccab27f2118990e366a1025d22
Alejandro Serrano <trupill@gmail.com>
2025-01-10 12:24:45 +0100

    Add default timeouts for JS and WASM (#90)

--- arrow-gradle-config-publish/src/main/kotlin/internal/JarTasks.kt
+++ arrow-gradle-config-publish/src/main/kotlin/internal/JarTasks.kt
@@ -6,46 +6,56 @@ import java.io.File
 import org.gradle.api.Project
 import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.api.tasks.bundling.Jar
-import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.register
 
 internal val Project.docsJar: Jar
   get() =
-    tasks.create<Jar>("docsJar") {
-      group = "build"
-      description = "Assembles Javadoc jar file from for publishing"
-      archiveClassifier.set("javadoc")
-      if (dokkaEnabled) {
-        tasks.findByName("dokkaHtml")?.let { dokkaHtml -> from(dokkaHtml) }
-      }
-    }
+    tasks
+      .register(
+        "docsJar",
+        fun Jar.() {
+          group = "build"
+          description = "Assembles Javadoc jar file from for publishing"
+          archiveClassifier.set("javadoc")
+          if (dokkaEnabled) {
+            tasks.findByName("dokkaHtml")?.let { dokkaHtml -> from(dokkaHtml) }
+          }
+        }
+      )
+      .get()
 
 internal val Project.sourcesJar: Jar
   get() =
-    tasks.create<Jar>("sourcesJar") {
-      group = "build"
-      description = "Assembles Sources jar file for publishing"
-      archiveClassifier.set("sources")
+    tasks
+      .register(
+        "sourcesJar",
+        fun Jar.() {
+          group = "build"
+          description = "Assembles Sources jar file for publishing"
+          archiveClassifier.set("sources")
 
-      val sources: Iterable<File> =
-        when {
-          isVersionCatalog -> emptySet()
-          isKotlinMultiplatform -> emptySet()
-          isJavaPlatform -> emptySet()
-          isKotlinJvm || isGradlePlugin -> {
-            (project.properties["sourceSets"] as SourceSetContainer)["main"].allSource
-          }
-          /*
-          isAndroidLibrary -> {
-            (project.extensions.getByName("android") as LibraryExtension)
-              .sourceSets
-              .named("main")
-              .get()
-              .java
-              .srcDirs
-          }
-          */
-          else -> emptySet()
+          val sources: Iterable<File> =
+            when {
+              isVersionCatalog -> emptySet()
+              isKotlinMultiplatform -> emptySet()
+              isJavaPlatform -> emptySet()
+              isKotlinJvm || isGradlePlugin -> {
+                (project.properties["sourceSets"] as SourceSetContainer)["main"].allSource
+              }
+              /*
+              isAndroidLibrary -> {
+                (project.extensions.getByName("android") as LibraryExtension)
+                  .sourceSets
+                  .named("main")
+                  .get()
+                  .java
+                  .srcDirs
+              }
+              */
+              else -> emptySet()
+            }
+          if (sources.toList().isNotEmpty()) from(sources)
         }
-      if (sources.toList().isNotEmpty()) from(sources)
-    }
+      )
+      .get()
